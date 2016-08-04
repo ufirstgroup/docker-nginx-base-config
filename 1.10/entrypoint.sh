04dc3852c07be7a0f7a8d3850cfda072d7c81321
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2016-08-04 11:17:30 +0200

    fix entrypoint

--- 1.10/entrypoint.sh
+++ 1.10/entrypoint.sh
@@ -3,6 +3,9 @@
 if [ ! -e "/etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem" ]; then
   openssl dhparam -out /etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem ${NGINX_DHPARAM_SIZE}
   rm -f /etc/nginx/base_config/dhparam.pem
+fi
+
+if [ ! -e "/etc/nginx/base_config/dhparam.pem" ]; then
   ln -s /etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem /etc/nginx/base_config/dhparam.pem
 fi
 

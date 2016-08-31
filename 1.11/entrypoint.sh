c49fadc209922f74cd06f93e17f43125fc002d10
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2016-08-31 16:52:12 +0200

    move dhparam files to conf.d

--- 1.11/entrypoint.sh
+++ 1.11/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ ! -e "/etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem" ]; then
-  openssl dhparam -out /etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem ${NGINX_DHPARAM_SIZE}
-  rm -f /etc/nginx/base_config/dhparam.pem
+if [ ! -e "/etc/nginx/conf.d/dhparam_${NGINX_DHPARAM_SIZE}.pem" ]; then
+  openssl dhparam -out /etc/nginx/conf.d/dhparam_${NGINX_DHPARAM_SIZE}.pem ${NGINX_DHPARAM_SIZE}
+  rm -f /etc/nginx/conf.d/dhparam.pem
 fi
 
-if [ ! -e "/etc/nginx/base_config/dhparam.pem" ]; then
-  ln -s /etc/nginx/base_config/dhparam_${NGINX_DHPARAM_SIZE}.pem /etc/nginx/base_config/dhparam.pem
+if [ ! -e "/etc/nginx/conf.d/dhparam.pem" ]; then
+  ln -s /etc/nginx/conf.d/dhparam_${NGINX_DHPARAM_SIZE}.pem /etc/nginx/conf.d/dhparam.pem
 fi
 
 exec "$@"
